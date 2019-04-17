ee3c2a25d2ade41bd4877e52830406b2a09feea9
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-04-17 14:28:19 -0500

    touched on ideas to improve color changes in ball.html

--- practiceProjects/ball.js
+++ practiceProjects/ball.js
@@ -1,3 +1,5 @@
+// I'm gonna make this function work such that it can switch between the default color of the page and the new color the user inputs. A work in progress, though. Alternate ideas may be arriving soon.
 function changeBackground(color) {
+   var oldColor = document.body.style.backgroundColor;
    document.body.style.backgroundColor = color;
 }
