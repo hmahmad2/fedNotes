a826381d489fd80ccc7b9fae33bdf6ab0706da52
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-07-21 15:00:59 -0500

    window object

---
+++ 05jsSandbox/15window.js
@@ -0,0 +1,31 @@
+let val;
+
+// OUTER HEIGHT AND WIDTH PROPERTIES
+val = window.outerHeight;
+val = window.outerWidth;
+
+// INNER HEIGHT AND WIDTH PROPERTIES
+val = window.innerHeight;
+val = window.innerWidth;
+
+// SCROLL POINTS PROPERTY
+val = window.scrollY;
+val = window.scrollX;
+
+// LOCATION OBJECT
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.search;
+
+// REDIRECT
+// window.location.href='https://www.google.com/'
+
+// RELOAD
+// window.location.reload();
+// window.history.go(-2);
+
+
+
+// OUTPUT
+console.log(val);
