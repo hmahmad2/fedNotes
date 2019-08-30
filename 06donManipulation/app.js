28da1bd1d62e260afb948357641aee89d4d2f314
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-08-30 13:30:31 -0500

    add task barely working, but some sort of working

--- 06donManipulation/app.js
+++ 06donManipulation/app.js
@@ -4,18 +4,17 @@ document.body.addEventListener('click', addItem);
 
 function deleteItem(e) {
    if (e.target.parentElement.classList.contains('delete-item')) {
-      console.log('Deleting item');
+      // console.log('Deleting item');
       e.target.parentElement.parentElement.remove();
    }
 }
 
 function deleteAllItems(e) {
    if (e.target.classList.contains('clear-tasks')) {
-      console.log("Deleting all items");
-      // for (var i = 0; i < )
+      // console.log("Deleting all items");
       let lis = document.querySelectorAll('li');
       lis.forEach(function(li) {
-         console.log(li.className);
+         // console.log(li.className);
          li.remove();
       });
    }
@@ -24,8 +23,24 @@ function deleteAllItems(e) {
 function addItem(e) {
    if (e.target.className === 'btn') {
       // let test = document.querySelector('#task-form').children[0].children[0].value;
-      let test = document.querySelector('#task').value;
-      console.log(test);
-      // This is how we get he value of the "New Task"!
+      let newTask = document.querySelector('#task').value;
+      // console.log(newTask);
+      // This is how we get the value of the "New Task"!
+
+      var listNode = document.createElement('li');
+      listNode.className = 'collection-item';
+      listNode.appendChild(document.createTextNode(newTask));
+
+      var link = document.createElement('a');
+      link.className = 'delete-item secondary-content';
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+
+      listNode.appendChild(link);
+      document.querySelector('ul.collection').appendChild(listNode);
+
+      // okay, this is not working, I can't add a different value otherwise they delete the old ones
+
+      // console.log(link);
+      // console.log(li);
    }
 }
