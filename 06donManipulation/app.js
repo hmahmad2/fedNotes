761f639bf958de5280dcadbb7dee4ece7523d1d9
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-09-01 14:28:57 -0500

    addItem function repaired

--- 06donManipulation/app.js
+++ 06donManipulation/app.js
@@ -1,6 +1,7 @@
 document.body.addEventListener('click', deleteItem);
 document.body.addEventListener('click', deleteAllItems);
 document.body.addEventListener('submit', addItem);
+// document.body.addEventListener('keydown', filterItems);
 
 function deleteItem(e) {
    if (e.target.parentElement.classList.contains('delete-item')) {
@@ -22,16 +23,19 @@ function deleteAllItems(e) {
 
 function addItem(e) {
    if (e.target.className === 'btn' || e.type === 'submit') {
+   // if (e.target.className === 'btn') {
       // let test = document.querySelector('#task-form').children[0].children[0].value;
-      let newTask = document.querySelector('#task').value;
+      // console.log('adding item');
+
+      const newTask = document.querySelector('#task').value;
       // console.log(newTask);
       // This is how we get the value of the "New Task"!
 
-      var li = document.createElement('li');
+      const li = document.createElement('li');
       li.className = 'collection-item';
       li.appendChild(document.createTextNode(newTask));
 
-      var link = document.createElement('a');
+      const link = document.createElement('a');
       link.className = 'delete-item secondary-content';
       link.innerHTML = '<i class="fa fa-remove"></i>';
 
@@ -40,7 +44,24 @@ function addItem(e) {
 
       // okay, this is not working, I can't add a different value otherwise they delete the old ones
 
-      console.log(link);
-      console.log(li);
+      // console.log(link);
+      // console.log(li);
+      e.preventDefault();
+      // Okay, adding this makes it work properly... what's the default?
    }
+
+
+
+   // function filterItems(e) {
+   //    // if (e.target.id === 'filter'){
+   //    //    console.log(e.target.id);
+   //    // }
+   //    var key = e.which || e.keyCode;
+   //    if (key === 13 && e.target.id === 'filter') {
+   //       console.log(e.target);
+   //       console.log('cat');
+   //    }
+   // }
+
+   // document.body.addEventListener('keydown', filterItems);
 }
